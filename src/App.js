394762ddd70a6dc730c72c18ad64600a629ee8c3
aligencay <ali.gencay42@gmail.com>
2024-12-01 19:51:00 +0300

    Finished Welcome and About Us sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import WelcomeCard from './components/WelcomeCard.jsx'
+import AboutUs from './components/AboutUs.jsx';
 
 function App() {
   return (
     <>
-    
+      <WelcomeCard />
+      <AboutUs />
     </>
   );
 }
---
+++ src/components/WelcomeCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import image from "../images/welcome-img.png"
+
+const Welcome = () => {
+  return (
+    <div className="relative w-full h-screen flex justify-center items-center">
+      <img
+        src={image}
+        alt="welcome-img"
+        className=""
+        style={{ opacity: 0.9 }}
+      />
+      <div className="absolute text-center text-white">
+        <h1 className="text-4xl font-bold mb-4">Welcome to World Devs</h1>
+        <p className="text-lg mb-6">
+          Innovating the future, one line of code at a time.
+        </p>
+        <button
+          style={{ backgroundColor: '#636AE8' }}
+          className="text-white px-6 py-3 rounded-lg"
+        >
+          Explore Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
