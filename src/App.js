fbd650c9c0f236c4b02dd888fc461e48d4de1f7c
aligencay <ali.gencay42@gmail.com>
2024-12-01 21:11:06 +0300

    photos added to cards

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import GetInTouch from './components/getInTouch/GetInTouch';
 import PersonCard from './components/person-card';
 import WelcomeCard from './components/WelcomeCard.jsx'
 import AboutUs from './components/AboutUs.jsx';
+// images
+import Ivan from "./images/ivan.jpg"
+import Deniz from "./images/deniz.jpg"
+import Ali from "./images/ali.jpg"
+import Markus from "./images/markus.jpg"
+import Cesar from "./images/cesar.jpg"
+
+
 
 function App() {
   return (
@@ -14,12 +22,12 @@ function App() {
       <WelcomeCard />
       <AboutUs />
       <div className='grid gap-6 px-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto bg-black' >
-        <PersonCard name={"Ivan Lopez"} role={"Software developer"} nationality={"Spanish"} linkedin={"https://www.linkedin.com/in/iván-lópez-2940742b8/"}/>
-        <PersonCard name={"Deniz Yetim"} role={"Software developer"} nationality={"Turkish"} linkedin={"https://www.linkedin.com/in/denizyetim/"}/>
-        <PersonCard name={"Ali Gencay"} role={"Software developer"} nationality={"Turkish"} linkedin={"https://www.linkedin.com/in/ali-gencay-417aa3241/"}/>
+        <PersonCard name={"Ivan Lopez"} role={"Software developer"} nationality={"Spanish"} linkedin={"https://www.linkedin.com/in/iván-lópez-2940742b8/"} photo={Ivan}/>
+        <PersonCard name={"Deniz Yetim"} role={"Software developer"} nationality={"Turkish"} linkedin={"https://www.linkedin.com/in/denizyetim/"} photo={Deniz}/>
+        <PersonCard name={"Ali Gencay"} role={"Software developer"} nationality={"Turkish"} linkedin={"https://www.linkedin.com/in/ali-gencay-417aa3241/"} photo={Ali}/>
         <PersonCard name={"Leonard Jedvaj"} role={"Software developer"} nationality={"Croatian"} linkedin={"https://www.linkedin.com/in/leonard-jedvaj-349313305/"}/>
-        <PersonCard name={"Markus Abramian Medina"} role={"Software developer"} nationality={"Spanish"} linkedin={"https://www.linkedin.com/in/markus-abramian-medina-1b0281273/"}/>
-        <PersonCard name={"César Aramis Suárez Orizondo"} role={"Software developer"} nationality={"Spanish"} linkedin={"https://www.linkedin.com/in/césar-aramis-suárez-orizondo/"}/>
+        <PersonCard name={"Markus Abramian Medina"} role={"Software developer"} nationality={"Spanish"} linkedin={"https://www.linkedin.com/in/markus-abramian-medina-1b0281273/"} photo={Markus}/>
+        <PersonCard name={"César Aramis Suárez Orizondo"} role={"Software developer"} nationality={"Spanish"} linkedin={"https://www.linkedin.com/in/césar-aramis-suárez-orizondo/"} photo={Cesar}/>
       </div>
       <GetInTouch />
       <Footer />  
